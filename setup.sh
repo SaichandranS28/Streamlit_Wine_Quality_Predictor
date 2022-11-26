9472190ff712555e47d930958611531b87e9bb68
SaichandranS28 <96342766+SaichandranS28@users.noreply.github.com>
2022-11-26 11:35:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 mkdir -p ~/.streamlit/
-
 echo "\
 [general]\n\
 email = \"email@domain\"\n\
 " > ~/.streamlit/credentials.toml
-
 echo "\
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
